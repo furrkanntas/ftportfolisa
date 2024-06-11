a874aa5d386cd78b371bc40dfcc74800f1e333c2
Furkan Taş <73059531+furrkanntas@users.noreply.github.com>
2024-06-12 02:07:58 +0300

    fixedeas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,11 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-
-        'xxs': '361px', // Özel ekran genişliği için 'xs' adında bir tanım, 400 piksel genişlik
-        'xfts': '389px', // Özel ekran genişliği için 'xs' adında bir tanım, 400 piksel genişlik
+        'iphonexr': {'raw': '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'},
+        'iphoneSE': {'raw': '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'},
+        'iphoneX': {'raw': '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'},
+        'iphone12pro': {'raw': '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)'},
+        'galaxys8plus': {'raw': '(device-width: 360px) and (device-height: 740px) and (-webkit-device-pixel-ratio: 4)'},
         'xs': '400px', // Özel ekran genişliği için 'xs' adında bir tanım, 400 piksel genişlik
         'sm': '640px',
         'md': '768px',
